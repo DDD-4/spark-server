e5fb3dfbb5096a42d1f3aa315611e7c0aa3700d9
ausleaf <seoyeong2352@gmail.com>
2020-09-01 23:53:10 +0900

    모두의 단어장 조회 API dummy 추가

--- src/main/kotlin/com/spark/poingpoing/vocabulary/VocabularyController.kt
+++ src/main/kotlin/com/spark/poingpoing/vocabulary/VocabularyController.kt
@@ -3,6 +3,8 @@ package com.spark.poingpoing.vocabulary
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.springframework.web.bind.annotation.*
+import java.io.File
+import java.net.InetAddress
 
 @Api(tags = ["Voca"])
 @RestController
@@ -30,8 +32,23 @@ class VocabularyController {
     @ApiOperation("모두의 단어장 조회")
     @GetMapping("v1/vocabularies/all")
     fun getAllVocabularies(): List<VocabularyResponse> {
+        val ip = InetAddress.getLocalHost().hostAddress
 
-        return listOf()
+        val dummyVocabulary1 = VocabularyResponse(
+                id = 1,
+                english = "apple",
+                korean = "사과",
+                photoUrl = "$ip:8080/v1/photos/apple.jpeg"
+        )
+
+        val dummyVocabulary2 = VocabularyResponse(
+                id = 2,
+                english = "desk",
+                korean = "책상",
+                photoUrl = "$ip:8080/v1/photos/desk.jpeg"
+        )
+
+        return listOf(dummyVocabulary1, dummyVocabulary2)
     }
 
     @ApiOperation("모두의 단어장 내려받기")
