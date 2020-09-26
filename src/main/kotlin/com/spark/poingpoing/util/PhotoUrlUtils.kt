e1f2f960259c44aca7aebe2c36d690146e98ec59
paige.k <paige.k@kakaocorp.com>
2020-09-27 01:22:52 +0900

    photo download key 생성 수정

--- src/main/kotlin/com/spark/poingpoing/util/PhotoUrlUtils.kt
+++ src/main/kotlin/com/spark/poingpoing/util/PhotoUrlUtils.kt
@@ -3,5 +3,5 @@ package com.spark.poingpoing.util
 private const val PHOTO_SERVER_HOST = "http://www.poingpoing.cf/v1/photos"
 
 fun String.convertToPhotoUrl(): String {
-    return "$PHOTO_SERVER_HOST/$this"
+    return "$PHOTO_SERVER_HOST/${this.toUUIDStringWithDash()}"
 }
