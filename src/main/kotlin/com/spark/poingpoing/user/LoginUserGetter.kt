bbe44c972699e0840508a67d8357539a0c26a66c
paige.k <paige.k@kakaocorp.com>
2020-10-06 08:40:26 +0900

    active 체크 추가

--- src/main/kotlin/com/spark/poingpoing/user/LoginUserGetter.kt
+++ src/main/kotlin/com/spark/poingpoing/user/LoginUserGetter.kt
@@ -1,5 +1,6 @@
 package com.spark.poingpoing.user
 
+import com.spark.poingpoing.exception.ForbiddenException
 import org.springframework.stereotype.Component
 import javax.servlet.http.HttpServletRequest
 
@@ -9,6 +10,11 @@ class LoginUserGetter(private val userRepository: UserRepository) {
     fun getLoginUser(httpServletRequest: HttpServletRequest): User {
         val defaultUserId = 1L
 
-        return userRepository.findById(defaultUserId).get()
+        val user =  userRepository.findById(defaultUserId).get()
+        if(!user.active) {
+           throw ForbiddenException("사용자를 찾을 수 없습니다.")
+        }
+
+        return user
     }
 }
